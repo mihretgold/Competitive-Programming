97d51b06af986646999d33a4ebb004145868347c
mihretgold <102969913+mihretgold@users.noreply.github.com>
2022-10-14 10:11:43 +0300

    Time: 330 ms (51.64%), Space: 79.9 MB (38.71%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    static bool cmp(pair<int,int>&a ,pair<int,int>&b){
+            return a.second>b.second;
+    }
+    
+    int minSetSize(vector<int>& arr) {
+      map<int,int>m;
+      int n=arr.size();
+      int counts=0,sum=0,i=0;
+       
+        for(auto i: arr){
+            m[i]++;
+        }
+         vector<pair<int,int>>v(m.begin(),m.end());
+   
+        sort(v.begin(), v.end(),cmp);
+        
+        while(sum <n/2){
+            sum+=v[i].second;
+            counts++;
+            i++;
+        }          
+        
+        return counts;
+    }
+};
