08087088d917975c3d07a2eb97e1b06384c879d6
mihretgold <102969913+mihretgold@users.noreply.github.com>
2022-10-21 19:02:25 +0300

    Time: 49 ms (34.47%), Space: 8.4 MB (61.17%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -46,7 +46,7 @@ public:
         return ans;
     }*/
     
-   // Run time 39
+   // Run time 18
     /*
     Algorithm
     1) initialize unordered map to hold each chat and its counts
@@ -56,32 +56,8 @@ public:
     5) we will first calculate the max length with r-l since index r is at the duplicate number
     6) while the element pointed by the right index is not equal to the element pointed by the left index(the duplicate)we increament the left pointer and decreament the count from the map
     7) then when we find the duplicate element and decrease the count on the map 
-    8)if there is no duplicate and/or if the right ptr == n-1 we put the max length as r-l+1 since the r element is not a duplicate
+    8)if there is no duplicate and/or if the right ptr == n-1 we put the max length as r-l+1 since the r element is not a duplicate    
     
-    
-    */
- /*  int lengthOfLongestSubstring(string s) {
-        int i=0, j=0, maxlen=0, len=0;
-        unordered_map<char,int> mymap;
-        while(j<s.length()){ 
-            if(mymap[s[j]]>0){ //if the count of the element is >0 or when we find a duplicate
-                len = j-i;
-                maxlen = max(maxlen, len);
-                while(s[j]!=s[i]){ //when index i is not the index holding the duplicate
-                    mymap[s[i]]--;//we decreament the count of the element
-                    i++;//and increament the index
-                }mymap[s[i]]--;
-                 i++;
-            }else if(j==s.length()-1){//at the end of the string
-                len = j-i+1;
-                maxlen = max(maxlen, len);
-            }
-            mymap[s[j]]++;//if the element is not a duplicate we store the element and count on the map
-            j++;
-        }
-        return maxlen;
-    }*/
-    /*
     012345678
     bcaaghji
         ^  ^
@@ -105,38 +81,47 @@ public:
            
        }
        return maxi;
- } 
+ }     
+    /*
+    Algorithm
+    1) declare a vector to push back non duplicate elements to
+    2) and 2 iterators one to find the duplicate and another to find index in vector v
+    3) add non duplicate elements to the vector
+    4) if duplicate element is found start count from 1 and initialize the 2nd iterator to the begining of the vector
+    5) until the 2nd iterator == 1st iterator(we find the duplicate element) we increament count and the 2nd iterator
+    6) if the duplicate is at the first element we erase the fisrst element if not we wrase the elements until the duplicate
+    7) add the new element to the vector
+    8) increment count and decreament erased elements
+    9) return count
     
-    
-    
- /*
- Run time 45ms
- int lengthOfLongestSubstring(string s) {
-     unordered_map<char, int> m; //character map to index
-     int maxi = 0, l = 0, r = 0;
-     
-     while (r < size(s)) {
-         if (m.find(s[r]) == m.end() || m[s[r]] < l) //element not duplicate
-             maxi = max(maxi, r-l+1);
-         else  l = m[s[r]] + 1;//element is duplicate
-         m[s[r++]] = r; //update element in map, and increment r;
-     }
-     return maxi;
- }*/
-  /*  
-  Run time 23ms
-  int lengthOfLongestSubstring(string s) {
-        vector<int> dict(256, -1);
-        int maxLen = 0, start = -1;
-        for (int i = 0; i != s.length(); i++) {
-            if (dict[s[i]] > start)// if duplicate found
-                start = dict[s[i]];
-            dict[s[i]] = i;
-            maxLen = max(maxLen, i - start);
+    */
+   /* int lengthOfLongestSubstring(string s) {
+       int n= s.length();
+       int c=0, m=0;
+        vector<char>v;
+        vector<char>::iterator it,it1;
+        for(int i=0; i<n; i++){//iterate through the string
+          it=find(v.begin(), v.end(), s[i]);//the iterator will find s[i] on the vector v
+            if(it!=v.end()){//if duplicate is found
+               int count =1;
+                it1=v.begin();
+                while(it1!=it){//while it1 is not a duplicate we increase the count
+                    it1++;
+                    count++;
+                }
+                if(it==v.begin()) v.erase(it);//if the duplicate element is on the the first element we erase it
+                else v.erase(v.begin(), ++it);//until we find the duplicate element we erase all the elements
+               v.push_back(s[i]);// add new element to the vector
+                c++;//increase count as we add an element to the vector
+                c-=count;// we decrease the elements that were erased
+            } 
+            else{//no duplicate
+                v.push_back(s[i]);// add non duplicate elements to the vector
+                c++;//increase count as we add an element to the vector
+            }
+            m= max(m,c);//calculate the max from all
         }
-        return maxLen;
-    }  */
-    
-    
+        return m;
+    }*/
     
 };
