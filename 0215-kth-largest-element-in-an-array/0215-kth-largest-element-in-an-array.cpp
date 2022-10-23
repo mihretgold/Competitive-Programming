4f34f4bdd73788eb0d9e7c078f00c6e47ab2495e
mihretgold <102969913+mihretgold@users.noreply.github.com>
2022-10-23 21:54:16 +0300

    Time: 235 ms (44.98%), Space: 47.4 MB (32.14%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,13 +1,34 @@
+/*class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) { 
+        priority_queue<int>p(nums.begin(),nums.end());// orders the elements in inreasing order
+       vector<int>v;
+        while(!p.empty()){
+            v.push_back(p.top());// puts the elements in deacreasing order 
+            p.pop();// pops the pushed elements so the next element can be pushed
+        }
+        int ans=v[k-1];//the (k-1)th index holds the kth largest element
+        return ans;
+    }
+};*/
+
+
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int>p(nums.begin(),nums.end());
-        vector<int>v;
-        while(!p.empty()){
-            v.push_back(p.top());
-            p.pop();
+    int findKthLargest(vector<int>& nums, int k) { 
+       priority_queue<int,vector<int>, greater<int>>p;
+        for(int x: nums){
+            //check if size of p is < k
+           if(p.size()<k){
+              p.push(x); 
+           } 
+            else{
+                 if(p.top()<x){//if size > k and x>p.top()
+                     p.pop();// replace the element
+                     p.push(x);
+                 }
+            }
         }
-        int ans=v[k-1];
-        return ans;
+        return p.top();
     }
 };
