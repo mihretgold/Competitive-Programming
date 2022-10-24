039c61ac02c4e2beacc452854b512943f445b8a8
mihretgold <102969913+mihretgold@users.noreply.github.com>
2022-10-24 13:28:52 +0300

    Time: 109 ms (14.86%), Space: 36.5 MB (84.79%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
-    int longestSubarray(vector<int>& nums) {
+    /*int longestSubarray(vector<int>& nums) {
         int n=nums.size(), counts=0,l=0,r=0;
         int m=0;
-        vector<int>::iterator i;
         while(r<n){
-            if(nums[r] == 0)
+            if(nums[r] == 0)//counts zeros
                 counts++;
-           while(counts>1){
+           while(counts>1){//while there is more than 1 zero increament left pointer
                if(nums[l]==0)
                    counts--;
                    l++;
@@ -15,6 +14,20 @@ public:
              r++;
         m=max(m,r-l-1);
            
+        }
+        return m;
+    }*/
+    int longestSubarray(vector<int>& nums) {
+        int n=nums.size(),count=0,m=0;
+        for(int i=0,j=0; i<n; i++){
+            if(nums[i]==0){
+               count++; 
+            }
+            while(count>1){
+                if(nums[j]==0)count--;
+                j++;
+            }
+            m=max(i-j,m);
         }
         return m;
     }
