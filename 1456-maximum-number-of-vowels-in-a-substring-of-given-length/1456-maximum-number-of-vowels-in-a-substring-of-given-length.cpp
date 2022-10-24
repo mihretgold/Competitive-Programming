39f86e493affdf00c7edfc75513279a74a763484
mihretgold <102969913+mihretgold@users.noreply.github.com>
2022-10-24 12:47:46 +0300

    Time: 67 ms (25.78%), Space: 9.9 MB (91.93%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
+    /*
+     s = "abciiidef", k = 3  count=0
+     //ask if all capital or lowercase letters
+             
+    */
     int maxVowels(string s, int k) {
-      int n=s.length();
-      int l=0, r=0,counts=0,m=0;
+      int n=s.size();
+        int l=0,r=0,count=0,m=0;
         while(r<n){
-            if(s[r]=='a'||s[r]=='e'||s[r]=='i'||s[r]=='o'||s[r]=='u'){
-                counts++;
-            }
-            if(r-l+1 >k){
-                if(s[l]=='a'||s[l]=='e'||s[l]=='i'||s[l]=='o'||s[l]=='u'){
-                counts--;
+            // add k number of elements to the window
+            while(r-l+1<=k){
+                if(s[r]=='a'|| s[r]=='e'|| s[r]=='i'|| s[r]=='o'|| s[r]=='u'){//if vowel increament count
+                    count++;
                 }
-                l++;
+                r++;
             }
-            r++;
-            m=max(m,counts);
+            m=max(m,count);
+             if(s[l]=='a'|| s[l]=='e'|| s[l]=='i'|| s[l]=='o'|| s[l]=='u'){//if vowel decreament count
+                    count--;
+                }
+            l++;
+            
         }
         return m;
     }
