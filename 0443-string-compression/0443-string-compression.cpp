316c38cce80da646c0799cd0472adcccd2a5df54
mihretgold <102969913+mihretgold@users.noreply.github.com>
2022-10-25 10:26:47 +0300

    Time: 16 ms (19.56%), Space: 8.9 MB (70.30%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,21 +1,41 @@
 class Solution {
 public:
+    /*
+                      l
+    ["a","a","b","b","c","c","c"]
+                              r
+    
+    ["a","2","b","2","c","3"]
+    
+    
+    //are the arrays sorted
+    */
+    /*
+    Algorithm
+    1) initialize l,r,k ptrs
+    2) while l<n
+    3) r=l
+    4) while r<n and elements on the right and left indexes are simillar increament r ptr
+    5) put the element on the k index ans increament the k ptr
+    6) put the elements count in the k ptr and increament k by converting int to_string()
+    7) move the left pointer to the new element
+    8) return k
+    */
     int compress(vector<char>& chars) {
-       int n=chars.size(),l=0,k=0;
+       int n=chars.size(),l=0,k=0,r=0;
        
         while(l<n){
-            int r=l;
-           while(r<n && chars[r]==chars[l]){
+           while(r<n && chars[r]==chars[l]){// counts same elements
                r++;
            }
            
-            chars[k++]=chars[l];
+            chars[k++]=chars[l];//puts the element on the l index on k index
             if(r-l>1){
-                for(char s: to_string(r-l)){
+                for(char s: to_string(r-l)){//apends the elements count after the element
                     chars[k++]=s;
                 }
             }
-            l=r;
+            l=r;//move the left pointer to the new element
         }
         
         return k;
