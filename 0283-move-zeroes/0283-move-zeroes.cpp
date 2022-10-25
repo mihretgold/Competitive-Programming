1af2aa838206637b54ef3a287592c3de356482f1
mihretgold <102969913+mihretgold@users.noreply.github.com>
2022-10-25 07:36:10 +0300

    Time: 48 ms (56.01%), Space: 19.1 MB (83.10%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -6,7 +6,7 @@ public:
     
     
     */
-    void moveZeroes(vector<int>& nums) {
+   /* void moveZeroes(vector<int>& nums) {
       int n=nums.size();
         vector<int>ans;
         int i,count=0;
@@ -20,9 +20,20 @@ public:
         for(i=0; i<count; i++){//we push back count number of 0's to the end
             ans.push_back(0);
         }
-        for(i=0;i<n; i++){
+        for(i=0;i<n; i++){//put all the elements on ans vector to nums
            nums[i]=ans[i];
         }
         //return ans;
+    }*/
+       void moveZeroes(vector<int>& nums) {
+        int n=nums.size(),r=0,l=0;
+           if(n==0||n==1) return;
+           while(r<n){
+               if(nums[r]==0){//if 0 move r ptr
+                   r++;
+               }else{//if not swap with the element at the l index and increament both r&l ptr
+                   swap(nums[l++], nums[r++]);
+               }
+           }
     }
 };
