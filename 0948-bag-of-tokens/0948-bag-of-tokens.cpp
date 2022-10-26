542c4ba2a48a04b922717f113b76af1ec3f7ffb4
mihretgold <102969913+mihretgold@users.noreply.github.com>
2022-10-26 08:19:14 +0300

    Time: 20 ms (13.93%), Space: 10.6 MB (69.44%) - LeetHub

--- 0948-bag-of-tokens/0948-bag-of-tokens.cpp
+++ 0948-bag-of-tokens/0948-bag-of-tokens.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
-    int bagOfTokensScore(vector<int>& tokens, int power) {
-        int l=0, r=tokens.size()-1, score=0;
+    /*
+    
+    [100,200,300,400], power =500 s=0
+    
+    //is it sorted
+    -sort
+    -if v[0]>p return 0
+    -else v[i]-p s++
+    s!=0 V[n-1]+p s--
+    [100,200], power = 50 s=1
+     
+    */
+   int bagOfTokensScore(vector<int>& tokens, int power) {
+        int n=tokens.size();
+        int s=0, e=n-1, score=0;
         
-        sort(tokens.begin(), tokens.end());
-        while(l<=r){
-            if(power >= tokens[l]){
-                power-=tokens[l++];
+        sort(tokens.begin(), tokens.end());//sort tokens
+        while(s<=e){
+            if(power >= tokens[s]){//if there is enough power for tokens to subtract from
+                power-=tokens[s++];
                 score++;
             }
-            else if(power+tokens[r]>=tokens[l] && score!=0 && r!=l){
-                power += tokens[r--];
+            else if(power+tokens[e]>=tokens[s] && score!=0 && e!=s){//if adding the tokens[e] element will get the score back or is >= tokens[s] element
+                power += tokens[e--];
                 score--;
             }
-            else{
+            else{//if it wont get the score back we break
                 break;
             }
         }
