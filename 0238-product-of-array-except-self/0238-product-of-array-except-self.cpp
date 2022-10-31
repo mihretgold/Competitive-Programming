09404d30e3734ac1259ba8a69dfca976d3ee77de
mihretgold <102969913+mihretgold@users.noreply.github.com>
2022-10-31 21:10:34 +0300

    Time: 56 ms (43.87%), Space: 24.1 MB (68.77%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -19,43 +19,39 @@ public:
         }
         return ans;
     }*/
-    vector<int> productExceptSelf(vector<int>& nums) {
+   /*USING PARTIAL SUM
+   vector<int> productExceptSelf(vector<int>& nums) {
        int n=nums.size();
         vector<int>pre(n);
          vector<int>suf(n);
         vector<int>ans(n);
-        pre[0]=1;
+        pre[0]=1;//start with idx 0 as 1 so the first element can be excluded and the ans coud be the suffix * 1
         suf[n-1]=1;
         
-        for(int i=1; i<n; i++){
+        for(int i=1; i<n; i++){//array of prefix
             pre[i]=pre[i-1]*nums[i-1];
         }
-        for(int i=n-2; i>=0; i--){
+        for(int i=n-2; i>=0; i--){//array of suffix
             suf[i]=suf[i+1]*nums[i+1];
         }
-         for(int i=0; i<n; i++){
+         for(int i=0; i<n; i++){//ans vectors
             ans[i]=pre[i]*suf[i];
         }
         return ans;
+    }*/
+    //DECREASED SPACE COMPLEXITY
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> answer(n, 1);
+        for (int i = 1; i < n; i++) {//prefix product
+            answer[i] = nums[i - 1] * answer[i - 1];
+        }
+        int prevProduct = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            int product = nums[i + 1] * prevProduct;//suffix product
+            answer[i] = answer[i] * product;//ans vector (prefix * suffix)
+            prevProduct = product;//storing sufix product of previous deacresing space complexity ot suffix array
+        }
+        return answer;
     }
 };
-/* public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int pre[] = new int[n];
-        int suff[] = new int[n];
-        pre[0] = 1;
-        suff[n - 1] = 1;
-        
-        for(int i = 1; i < n; i++) {
-            pre[i] = pre[i - 1] * nums[i - 1];
-        }
-        for(int i = n - 2; i >= 0; i--) {
-            suff[i] = suff[i + 1] * nums[i + 1];
-        }
-        
-        int ans[] = new int[n];
-        for(int i = 0; i < n; i++) {
-            ans[i] = pre[i] * suff[i];
-        }
-        return ans;
-    }*/
