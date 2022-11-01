182901c849d36c44084767ff881f115357816882
mihretgold <102969913+mihretgold@users.noreply.github.com>
2022-11-01 13:23:36 +0300

    Time: 27 ms (80.03%), Space: 19.5 MB (60.20%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -16,17 +16,27 @@ public:
         
         return max;
     }*/
-    
+    /* TIMECOMPLEXITY: O(NlogN) SPACE: O(1)   
     int majorityElement(vector<int>& nums) {
         int n=nums.size();
         sort(nums.begin(),nums.end());        
         return nums[n/2];
-    }
-    
-    /*int majorityElement(vector<int>& nums) {
-        int max=nums[0];//store first element as maximum
-        
+    }*/
+     //TIMECOMPLEXITY: O(N) SPACE: O(1)  
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        int max=nums[0],count=1;//store first element as max and start count as 1
+        for(int i=1; i<n; i++){
+            if(max==nums[i]){//if next element is same as max increament count
+                count++;
+            }else if(count==0){//if count becomes 0 store the element as max and increament count
+                count++;
+                max=nums[i];
+            }else{//if next element is not equal to max deacreament count
+                count--;
+            }
+        }
         
         return max;
-    }*/
+    }
 };
