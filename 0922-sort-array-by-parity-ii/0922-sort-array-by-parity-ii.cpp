697333bb5c831cc0908dfd06c305652718812cd5
mihretgold <102969913+mihretgold@users.noreply.github.com>
2022-11-04 16:03:49 +0300

    Time: 57 ms (21.65%), Space: 21.3 MB (97.87%) - LeetHub

--- 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.cpp
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.cpp
@@ -1,20 +1,38 @@
 class Solution {
 public:
-    vector<int> sortArrayByParityII(vector<int>& nums) {
+   /* SPACE COMPLEXITY O(N)
+   vector<int> sortArrayByParityII(vector<int>& nums) {
         vector<int>even;
         vector<int>odd;
         vector<int>ans;
       for(int i=0; i<nums.size(); i++){
-         if(nums[i]%2==1){
+         if(nums[i]%2==1){//add odd nums to odd array
            odd.push_back(nums[i]);
-         }else{
+         }else{// add even nums to even array
              even.push_back(nums[i]);
          } 
       }
-        for(int i=0; i<even.size(); i++){
+        for(int i=0; i<even.size(); i++){//add even then odd nums to ans array
             ans.push_back(even[i]);
             ans.push_back(odd[i]);
         }
         return ans;
+    }*/
+    // SPACE COMPLEXITY O(1)
+    vector<int> sortArrayByParityII(vector<int>& nums) {
+        int i=0,j=1,n=nums.size();
+        while(i<n && j<n){
+            if(nums[i]%2==0){//if even
+                i+=2;
+            }else if(nums[j]%2!=0){//if odd
+                j+=2;
+            }else{//if in wrong order
+                swap(nums[j],nums[i]);
+                i+=2;
+                j+=2;
+            }
+        }
+        
+        return nums;
     }
 };
