0ca09327fd859346c60495de960b7a912f61366a
mihretgold <102969913+mihretgold@users.noreply.github.com>
2022-11-05 11:36:06 +0300

    Time: 316 ms (53.27%), Space: 74.4 MB (72.23%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.cpp
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.cpp
@@ -1,17 +1,28 @@
 #define ll long long;
 class Solution {
 public:
-    int chalkReplacer(vector<int>& chalk, int k) {
+   /* int chalkReplacer(vector<int>& chalk, int k) {
         int n = chalk.size();
         long sum = 0;
-        for (auto x : chalk) sum += x;
-        k = k % sum;
+        for (auto x : chalk) sum += x;//sum of all elements in the array
+        k = k % sum;//to simplify reapeated subtraction
         int i = 0;
-        while (k >= chalk[i]) {
+        while (k >= chalk[i]) {//while k > the elements in the array we increament i
             k -= chalk[i];
-            i = (i + 1) % n;
+            i = (i + 1) % n;//if i becomes greater than n
         }
         
         return i;
+    }*/
+     int chalkReplacer(vector<int>& chalk, int k) {
+       int n=chalk.size(),i=0;
+         long sum=0;
+         for(int x: chalk) sum+=x;
+         k%=sum;
+         while(k>=chalk[i]){
+             k-=chalk[i];
+             i=(++i)% n;
+         }
+         return i;
     }
 };
