9a4a0f3fb8b69a153311a275e07408f2c96a7728
mihretgold <102969913+mihretgold@users.noreply.github.com>
2022-11-07 13:54:42 +0300

    Time: 0 ms (100.00%), Space: 6.4 MB (51.91%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    string reverseParentheses(string s) {
-    stack<char> a;
-   
-    int n = s.size();
-        for(int i=0; i<n; i++){
-          if(s[i]!=')')
-            a.push(s[i]);
-        else{
+    string reverseParentheses(string s){
+    int n=s.size();
+    stack<char>st;
+    for(int i=0; i<n; i++){
+        //find first 
+        if(s[i]==')'){
              string word="";
-        while(!a.empty()&&a.top()!='('){
-              word+=a.top();
-              a.pop();
-            } 
-            a.pop();
-            for(int i=0; i<word.size(); i++)
-                a.push(word[i]);
-          }
+            while(!st.empty() && st.top() != '('){//to only reverse the elements in the brace
+               word+=st.top();
+                st.pop();
+            }
+            st.pop();//to reve the open bracket
+            for(char x: word)
+              st.push(x);
+        }else{
+          st.push(s[i]);
         }
-        string ans="";
-        while(!a.empty()){
-            ans+= a.top();
-            a.pop();
-        }
-        reverse(ans.begin(), ans.end());
-      return ans;  
     }
+        string ans ="";
+        while(!st.empty()){//put the reverse elements in the string ans
+           ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());//reverse to get orignal answer
+    return ans;
+}
 };
