d73293df3faa76108061e074477255a0fb391eb2
mihretgold <102969913+mihretgold@users.noreply.github.com>
2022-11-08 09:54:36 +0300

    Time: 10 ms (40.03%), Space: 6.8 MB (35.34%) - LeetHub

---
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int secondHighest(string s) {
+       int n=s.size(),k=2;
+        unordered_map<char,int>m;
+        priority_queue<int,vector<int>,greater<int>>pq;
+        for(int i=0; i<n; i++){
+            if(s[i]>= 48 && s[i]<=57){
+                m[s[i]-48]++;
+            }
+        }
+        for(auto c: m){
+            int i=c.first;
+            if(pq.size()< k){
+                pq.push(i);
+            }else{
+                if(i>pq.top()){
+                    pq.pop();
+                    pq.push(i);
+                }
+            }
+        }
+        if(pq.size()!= k) return -1;
+        return pq.top();
+    }
+};
