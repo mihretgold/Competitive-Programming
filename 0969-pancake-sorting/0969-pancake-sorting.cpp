20ec036c82ef5c32fbf34e45bb320cfec079320a
mihretgold <102969913+mihretgold@users.noreply.github.com>
2022-11-09 08:20:03 +0300

    Time: 14 ms (14.37%), Space: 11 MB (87.25%) - LeetHub

--- 0969-pancake-sorting/0969-pancake-sorting.cpp
+++ 0969-pancake-sorting/0969-pancake-sorting.cpp
@@ -1,15 +1,55 @@
 class Solution {
 public:
-    vector<int> pancakeSort(vector<int>& arr) {
+    /*vector<int> pancakeSort(vector<int>& arr) {
        vector<int> ans;
-        int j,i;
-        for (i = arr.size(); i > 0; --i) {
-            for (j = 0; arr[j] != i; ++j);
-            reverse(arr.begin(), arr.begin() + j + 1);
-            ans.push_back(j + 1);
-            reverse(arr.begin(), arr.begin() + i);
-            ans.push_back(i);
+        //almost like bubble sort
+        for (int i = arr.size()-1; i > 0; --i) {
+            for (int j = 1; j <= i; ++j){//we start j from 1 so that it skips if the number is on the right spot
+                if(arr[j]==i+1){//since the max num == the size of the array or i+1
+                    reverse(arr.begin(),arr.begin()+j);//when we find the max we flip it to the front
+                    ans.push_back(j+1);
+                    break;
+                }
+            }
+             reverse(arr.begin(),arr.begin()+i);// then we flip the whole thing bring it to the end
+             ans.push_back(i+1); 
         }
-        return ans;  
+        return ans;  */
+        /* vector<int> pancakeSort(vector<int>& arr) {
+        vector<int>ans;
+        for(int i = arr.size() - 1;i>=0; i--){
+            if(arr[i] != i+1){
+                //Find i+1
+                int j = 0;
+                while(arr[j] != i+1) j++;
+                //Reverse 0,j
+                ans.push_back(j+1);reverse(arr.begin(),arr.begin()+j);
+                
+                //Reverse 0,i
+                reverse(arr.begin(),arr.begin()+i);ans.push_back(i+1);
+            }
+        }
+        return ans;*/
+     void reverse(int l,int r,vector<int>&arr){
+        while(l < r){
+            swap(arr[l],arr[r]);
+            l++;r--;
+        }
+    }
+    vector<int> pancakeSort(vector<int>& arr) {
+        vector<int>res;
+        for(int i = arr.size() - 1;i>=0; i--){
+            if(arr[i] != i+1){
+                //Find i+1
+                int k = 0;
+                while(arr[k] != i+1) k++;
+                //Reverse 0,k
+                res.push_back(k+1);reverse(0,k,arr);
+                
+                //Reverse 0,i;
+                reverse(0,i,arr);res.push_back(i+1);
+            }
+        }
+        return res;
     }
 };
