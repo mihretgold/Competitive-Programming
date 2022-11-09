23a25d332c4c03fb80871cb1b1b047a6d48d209a
mihretgold <102969913+mihretgold@users.noreply.github.com>
2022-11-09 08:35:10 +0300

    Time: 8 ms (50.20%), Space: 11.1 MB (87.25%) - LeetHub

--- 0969-pancake-sorting/0969-pancake-sorting.cpp
+++ 0969-pancake-sorting/0969-pancake-sorting.cpp
@@ -1,54 +1,17 @@
 class Solution {
 public:
-    /*vector<int> pancakeSort(vector<int>& arr) {
-       vector<int> ans;
-        //almost like bubble sort
-        for (int i = arr.size()-1; i > 0; --i) {
-        if(arr[i] != i+1){
-            for (int j = 1; j <= i; ++j){//we start j from 1 so that it skips if the number is on the right spot
-                if(arr[j]==i+1){//since the max num == the size of the array or i+1
-                    reverse(arr.begin(),arr.begin()+j);//when we find the max we flip it to the front
-                    ans.push_back(j+1);
-                     reverse(arr.begin(),arr.begin()+i);// then we flip the whole thing bring it to the end
-             ans.push_back(i+1); 
-                    break;
-                }
-            }
-             }
-        }
-        return ans;  
-        /* vector<int> pancakeSort(vector<int>& arr) {
-        vector<int>ans;
-        for(int i = arr.size() - 1;i>=0; i--){
-            if(arr[i] != i+1){
-                //Find i+1
-                int j = 0;
-                while(arr[j] != i+1) j++;
-                //Reverse 0,j
-                ans.push_back(j+1);reverse(arr.begin(),arr.begin()+j);
-                
-                //Reverse 0,i
-                reverse(arr.begin(),arr.begin()+i);ans.push_back(i+1);
-            }
-        }
-        return ans;*/
-    /* void reverse(int l,int r,vector<int>&arr){
-        while(l < r){
-            swap(arr[l],arr[r]);
-            l++;r--;
-        }
-    }*/
     vector<int> pancakeSort(vector<int>& arr) {
         vector<int>res;
         for(int i = arr.size() - 1;i>=0; i--){
-            if(arr[i] != i+1){
+            //since the size of the array is the max number
+            if(arr[i] != i+1){//if the last element is not the max num b/c it would already be in it's place
                 //Find i+1
                 int k = 0;
-                while(arr[k] != i+1) k++;
-                //Reverse 0,k
+                while(arr[k] != i+1) k++;//increament k until==i+1
+                //Reverse 0,k puting i+1 element to the front
                 res.push_back(k+1);reverse(arr.begin(),arr.begin()+k+1);
                 
-                //Reverse 0,i;
+                //Reverse 0,i putting i+1 element at the end
                reverse(arr.begin(),arr.begin()+i+1);res.push_back(i+1);
             }
         }
