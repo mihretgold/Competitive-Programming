be6f8389274cfb65389d5ad7adaf71f0fd1ea352
mihretgold <102969913+mihretgold@users.noreply.github.com>
2022-11-09 13:22:06 +0300

    Time: 134 ms (93.40%), Space: 53.4 MB (92.36%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.cpp
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.cpp
@@ -3,7 +3,7 @@ public:
     int maxCoins(vector<int>& piles) {
       int n=piles.size(),sum=0,counts=0;
         sort(piles.begin(),piles.end());
-        int k=n/3,i=1;
+        //we do n/3 to leave 1/3 of the elements to bob and iterate the rest 2/3 with Alison
         for(int i=n/3; i<n; i+=2){
             sum+=piles[i];
         }
