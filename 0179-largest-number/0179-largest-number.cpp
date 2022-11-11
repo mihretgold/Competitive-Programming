a704dda8dc6dfd263bd832191d98a722a0bd214e
mihretgold <102969913+mihretgold@users.noreply.github.com>
2022-11-11 17:55:13 +0300

    Time: 18 ms (52.76%), Space: 11.2 MB (64.68%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    static bool cmp(string &a, string &b){
+    /*static bool cmp(string &a, string &b){
         return (a+b)>=(b+a);
     }
     string largestNumber(vector<int>& nums) {
         vector<string>s(nums.size(),"");
         int x=0;
-        for(int i=0; i<nums.size(); i++){
+        for(int i=0; i<nums.size(); i++){//keep count for zeros
            if(nums[i]==0){
                x++;
            }
         }
-        if(x==nums.size()) return "0";
+        if(x==nums.size()) return "0";//if all elements are 0 we return "0"
         
         for(int i=0; i<nums.size(); i++){
             s[i]=to_string(nums[i]);
@@ -21,6 +21,26 @@ public:
         for(int i=0; i<s.size(); i++){
             ans+=s[i];
         }
-        return ans;
+        return ans;*/
+    static bool cmp(string &a, string&b){
+    return a+b>=b+a;
+}
+string largestNumber(vector<int>& nums){
+   int n=nums.size(),count=0;
+    for(int x: nums){
+        if(x==0) count++;
     }
+    //if all elements are 0
+    if(count ==n) return "0";
+    vector<string>s(n,"");
+    for(int i=0; i<n; i++){
+        s[i]+=to_string(nums[i]);
+    }
+    sort(s.begin(),s.end(),cmp);
+    string ans="";
+    for(int i=0; i<n; i++){
+        ans+=s[i];
+    }
+    return ans;
+}
 };
