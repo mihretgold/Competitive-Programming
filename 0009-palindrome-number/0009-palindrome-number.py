aaf4ef120dbb0ffaf9ff74315f6fb9d318985c99
Mihret Agegnehu <102969913+mihretgold@users.noreply.github.com>
2022-12-21 16:17:06 +0300

    Time: 186 ms (7.64%), Space: 13.9 MB (16.12%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -2,15 +2,15 @@ class Solution:
     # Algorithm
     # s1: reverse number
     # s2: if number is same as the original return true else return false
-    def isPalindrome(self, x: int) -> bool:
-        if x<0 or x%10==0 and x!=0:
-            return False
-        rev=0
-        while(x>rev):
-            r=x%10
-            rev=(rev*10) + r
-            x=x//10
-        if x==rev or x==rev//10:
-            return True
-        else: return False
-    
+     def isPalindrome(self, x: int) -> bool:
+            if x<0:
+                return False
+            
+            num_original=x
+            rev=0
+            while(x>0):
+                r=x%10
+                rev=(rev*10) + r
+                x//=10
+            return num_original==rev
+        
