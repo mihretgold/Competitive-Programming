c9284c648a7187ba7666c3ce430f9129a3df28ec
Mihret Agegnehu <102969913+mihretgold@users.noreply.github.com>
2022-12-27 18:24:00 +0300

    Create set_diffrence.py

---
+++ set_diffrence.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+english_size = int(input())
+english = set(map(int,input().split(' ')))
+french_size = int(input())
+french = set(map(int,input().split(' ')))
+only_english = {}
+only_english = english.difference(french)
+print(len(only_english))
