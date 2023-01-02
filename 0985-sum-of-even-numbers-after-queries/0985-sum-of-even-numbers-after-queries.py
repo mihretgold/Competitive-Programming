335c4942e20c5a5b50e80b643c6c908fec222b62
Mihret Agegnehu <102969913+mihretgold@users.noreply.github.com>
2023-01-02 08:46:18 +0300

    Time: 539 ms (71.22%), Space: 20.5 MB (41.84%) - LeetHub

--- 0985-sum-of-even-numbers-after-queries/0985-sum-of-even-numbers-after-queries.py
+++ 0985-sum-of-even-numbers-after-queries/0985-sum-of-even-numbers-after-queries.py
@@ -2,13 +2,17 @@ class Solution:
     def sumEvenAfterQueries(self, nums: List[int], queries: List[List[int]]) -> List[int]:
         answer = []
         sum = 0
+        #Calculate sum before changes
         for num in nums:
-                if num % 2 == 0:
-                    sum += num 
+            if num % 2 == 0:
+                sum += num 
+                
         for val in queries:
+            #if new value being replaced is even we subtract it
             if nums[val[1]] % 2 == 0:
                 sum -= nums[val[1]]
             nums[val[1]] = nums[val[1]] + val[0]
+            #if new value being replaced is even we add it
             if nums[val[1]] % 2 == 0:
                 sum += nums[val[1]]
             answer.append(sum)
