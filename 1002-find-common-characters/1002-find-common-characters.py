6d761d7f6833c310337fc65dde8bfdee258e5c62
Mihret Agegnehu <102969913+mihretgold@users.noreply.github.com>
2023-01-16 09:55:17 +0300

    Time: 49 ms (87.25%), Space: 14 MB (22.46%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.py
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -1,30 +1,18 @@
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
-        lengthList = len(words)
-        list_check = list(words[0])
-        list_check.sort()
         answer = []
+        string = words[0]
+        length = len(words)
         
-        for index in range(1, lengthList):
-            list_word = list(words[index])
-            list_word.sort()
+        for index in range(1, length):
+            length_words = len(words[index])
             temp = ''
-            length_ans = len(list_check)
-            length_word = len(list_word)
-            i = 0
-            j = 0
-            while i < length_ans and j < length_word:
-                if list_word[j] == list_check[i]:
-                    temp += list_word[j]
-                    i += 1
-                    j +=1
-                elif list_word[j] < list_check[i]:
-                    j += 1
-                elif list_word[j] > list_check[i]:
-                    i += 1 
-            list_check = list(temp)
-            
-        for letter in list_check:
-            answer.append(letter)
+            for letter in range(length_words):
+                if words[index][letter] in string:
+                    temp += words[index][letter]
+                    string = string.replace(words[index][letter], "", 1)
+            string = temp
+        
+        answer = list(string)
         
         return answer
