3ad5650f16348a5adb692de9f857bb444bc3c1e6
Mihret Agegnehu <102969913+mihretgold@users.noreply.github.com>
2023-01-24 06:29:39 +0300

    Time: 39 ms (84.65%), Space: 14 MB (18.35%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -1,28 +1,29 @@
 class Solution:
     def customComparator(self, a: str, b: str):
+        ans = - 1
         if (a+b) < (b+a):
-            return 1
+            ans = 1
         elif (a+b) == (b+a):
-            return 0
-        else:
-            return -1
+            ans = 0
+        
+        return ans
         
     def largestNumber(self, nums: List[int]) -> str:
         length = len(nums)
         list_string = []
-        count_zero = 0
+        # count_zero = 0
         for num in nums:
-            if num == 0:
-                count_zero += 1
+            # if num == 0:
+            #     count_zero += 1
             list_string.append(str(num))
             
-        if count_zero == length:
-            return "0"
+        # if count_zero == length:
+        #     return "0"
         
         sorted_list = sorted(list_string, key = cmp_to_key(self.customComparator));
         # list_string.sort(key = cmp_to_key(self.customComparator));
         
-        String = "".join(sorted_list)
+        String =str(int("".join(sorted_list)))
             
         return String
             
