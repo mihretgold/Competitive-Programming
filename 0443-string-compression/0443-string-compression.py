ec97e95bc2d6753bf9eebba8cddb8b5dcf40fb90
Mihret Agegnehu <102969913+mihretgold@users.noreply.github.com>
2023-02-01 20:18:59 +0300

    Time: 67 ms (51.57%), Space: 13.9 MB (97.14%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -1,37 +1,48 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        left = 0
         length = len(chars)
         index = 0
-        count = 0
+        i = 0
         
-        for right in range(length):
-            if chars[left] != chars[right]:
-                chars[index] = chars[left]
-                index += 1
-                count += 1
-                if right - left > 1:
-                    k = str(right - left)
-                    for num in k:
-                        chars[index] = num
-                        index += 1
-                        count += 1
-                if right == length-1:
-                    chars[index] = chars[right]
+        while i < length:
+            j = i + 1
+            while j < length and chars[i] == chars[j]:
+                j += 1
+            
+            chars[index] = chars[i]
+            index += 1
+            count = str(j - i)
+            if count != '1':
+                for k in count:
+                    chars[index] = k
                     index += 1
-                    count += 1
-                left = right 
-            elif right == length-1:
-                chars[index] = chars[right]
-                index += 1
-                count += 1
-                if right - left > 0:
-                    k = str(right - left + 1)
-                    for num in k:
-                        chars[index] = num
-                        index += 1
-                        count += 1
-                left = right 
+            i = j
+            
+        return index
+        
+        # for right in range(length):
+        #     if chars[left] != chars[right]:
+        #         chars[index] = chars[left]
+        #         index += 1
+        #         if right - left > 1:
+        #             k = str(right - left)
+        #             for num in k:
+        #                 chars[index] = num
+        #                 index += 1
+        #         left = right 
+            #     if right == length-1:
+            #         chars[index] = chars[right]
+            #         index += 1
+            #     left = right 
+            # elif right == length-1:
+            #     chars[index] = chars[right]
+            #     index += 1
+            #     if right - left > 0:
+            #         k = str(right - left + 1)
+            #         for num in k:
+            #             chars[index] = num
+            #             index += 1
+            #     left = right 
                 
        
             
