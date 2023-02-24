6a4cd9819d94928fc75dd9b6d0cb78e8344c2a30
Mihret Agegnehu <102969913+mihretgold@users.noreply.github.com>
2023-02-24 14:12:12 +0300

    Time: 1456 ms (50.56%), Space: 27.4 MB (98.46%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,36 @@
+class Solution:
+    '''
+    Algo
+    1) start from back append index 0 last element to stack
+    2) if next element is greater pop until it's not 
+    3) put the last top element index - now index in list
+    4) then push index of element to stack
+    
+    dict = {(73, 1) 74,}
+    stack = [6, 2]
+    [0, 0, 1,1,2,4,1,1 ]
+    stack = [8, 7, 6]
+    ''' 
+    
+    
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        answer = []
+        length = len(temperatures)
+        
+        for index in range(length-1, -1, -1):
+            # if len(stack) == 0:
+            #     stack.append(temperatures[index])                
+            while len(stack) > 0 and temperatures[index] >= temperatures[stack[-1]]:
+                stack.pop()
+            if len(stack) > 0:
+                warmer = stack[-1] - index
+                answer.append(warmer)
+            else:
+                answer.append(0)
+            stack.append(index)
+        answer.reverse()
+            
+        return answer
+            
+            
