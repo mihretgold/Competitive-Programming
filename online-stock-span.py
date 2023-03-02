89dd867d851fcc391be60355bc7146ca4aa688ce
Mihret Agegnehu <102969913+mihretgold@users.noreply.github.com>
2023-03-02 22:33:54 +0300

    added online-stock-span.python

--- online-stock-span.py
+++ online-stock-span.py
@@ -2,21 +2,13 @@ class StockSpanner:
 
     def __init__(self):
         self.stock = []
-        self.count = 0
 
     def next(self, price: int) -> int:
-        top = 0
         ans = 1
         while self.stock and self.stock[-1][0] <= price:
-            self.stock.pop() 
-
-        if self.stock:
-            top = self.stock[-1][1]
-            ans = self.count - top
-        else:
-            ans = self.count + 1
-        self.stock.append((price, self.count))
-        self.count += 1
+            ans += self.stock.pop()[1]
+        
+        self.stock.append((price, ans))
         
         return ans
 
