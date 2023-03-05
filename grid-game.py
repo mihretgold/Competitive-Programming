fe391a9cfa6cf3befad6a2cb8fad23761ce43a33
Mihret Agegnehu <102969913+mihretgold@users.noreply.github.com>
2023-03-05 18:30:07 +0300

    added grid-game.python

--- grid-game.py
+++ grid-game.py
@@ -1,15 +1,23 @@
 class Solution:
     def gridGame(self, grid: List[List[int]]) -> int:
         length = len(grid[0])
-        suffix = list(accumulate(reversed(grid[0]), initial = 0))
-        prefix = list(accumulate(grid[1], initial = 0))
-        suffix.reverse()
         mini = float('inf')
 
+        for index in range(length-2, -1, -1):
+            grid[0][index] += grid[0][index+1]
+        
+        for index in range(1, length):
+            grid[1][index] += grid[1][index-1]
+
+
         for index in range(length):
-            choice1 = prefix[index]
-            choice2 = suffix[index + 1]
+            choice1 = 0
+            if index > 0:
+                choice1 = grid[1][index-1]
+            choice2 = 0
+            if index < length-1:
+                choice2 = grid[0][index + 1]
             choice = max(choice1, choice2)
             mini = min(mini, choice)
-            
+
         return mini
