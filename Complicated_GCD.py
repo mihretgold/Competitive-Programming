27f51b52c7a72ba64f073c6d37510fdf3bffaee8
Mihret Agegnehu <102969913+mihretgold@users.noreply.github.com>
2023-04-04 08:12:18 +0300

    added Complicated_GCD.py

---
+++ Complicated_GCD.py
@@ -0,0 +1,20 @@
+def I(): return int(input())
+def II(): return map(int, input().split())
+def IL(): return list(map(int, input().split()))
+def SIL(): return sorted(map(int, input().split()))
+from collections import defaultdict
+from collections import Counter
+
+def solve():
+    a, b = map(str, input().split())
+    if a == b:
+        print(a)
+    else:
+        print(1)
+
+
+
+
+T = 1
+for ___ in range(T):
+    solve()
