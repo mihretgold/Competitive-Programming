b0383d8c58f2f9eba1165a1b9030b4dd20071de2
Mihret Agegnehu <102969913+mihretgold@users.noreply.github.com>
2023-05-03 08:07:04 +0300

    added maximum-number-of-achievable-transfer-requests.python

--- maximum-number-of-achievable-transfer-requests.py
+++ maximum-number-of-achievable-transfer-requests.py
@@ -9,12 +9,13 @@ class Solution:
                 return num_requests
 
             froms, to = requests[index]
-            temp = net_change[:]
             net_change[froms] -= 1
             net_change[to] += 1
 
             all_buldings = calcMaxReq(index + 1, net_change[:], num_requests+1)
-            without_last = calcMaxReq(index + 1, temp[:], num_requests)
+            net_change[froms] += 1
+            net_change[to] -= 1
+            without_last = calcMaxReq(index + 1, net_change[:], num_requests)
 
             return max(all_buldings, without_last)
 
