740ea65fa7117bc754547958e58f681dc472900a
Mihret Agegnehu <102969913+mihretgold@users.noreply.github.com>
2023-05-07 09:56:41 +0300

    added split-array-into-consecutive-subsequences.python

--- split-array-into-consecutive-subsequences.py
+++ split-array-into-consecutive-subsequences.py
@@ -1,44 +1,30 @@
 class Solution:
     def isPossible(self, nums: List[int]) -> bool:
         length = len(nums)
-        heap = []
-        heap2 = []
-        mini = float('inf')
-        popped = []
+        heap = [(nums[0], 1)]
         check = True
-        for i in range(length):
-            if heap:
-                print(nums[i], heap[0][0], heap2[0][1])
-            if heap and  (nums[i] - heap[0][0] == 1 or nums[i] - heap2[0][1] == 1):
-                # print(heap)
-                # print(nums[i], heap[0][0], heap2[0][1])
-                if nums[i] - heap[0][0] == 1:
-                    # print(num, index)
-                    num, index = heappop(heap)
-                    heap2.remove((index, num))
-                    heappush(heap, (nums[i], index+1))
-                    heappush(heap2, (index+1, nums[i]))
-                else:
-                    index, num = heappop(heap2)
-                    heap.remove((num, index))
-                    heappush(heap, (nums[i], index+1))
-                    heappush(heap2, (index+1, nums[i]))
-            
+        for i in range(1, length):
+            if nums[i] - heap[0][0] == 1:
+                num, index = heappop(heap)
+                heappush(heap, (nums[i], index+1))  
+            elif nums[i] == heap[0][0]:
+                heappush(heap, (nums[i], 1))         
             else:
-                if heap and nums[i] > heap[0][0] and heap[0][1] >= 3:
+                found = False
+                while heap and not found:
                     num, index = heappop(heap)
-                    heap2.remove((index, num))
-                elif heap and nums[i] > heap[0][0] and heap[0][1] < 3:
-                    return False
-                heappush(heap, (nums[i], 1))
-                heappush(heap2, (1, nums[i]))
-        print(heap)
+                    if nums[i] - num == 1:
+                        heappush(heap, (nums[i], index+1))
+                        found = True
+                    elif index < 3:
+                        check = False
+
+                if not found:
+                    heappush(heap, (nums[i], 1))
+        
         for a, b in heap:
             if b < 3:
                 check = False
-                break 
-            
-           
-            
+                break         
 
         return check
