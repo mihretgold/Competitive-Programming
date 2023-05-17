ff884535d319b105a750ad41c72c0cbb03801fc8
Mihret Agegnehu <102969913+mihretgold@users.noreply.github.com>
2023-05-17 16:08:21 +0300

    added find-if-path-exists-in-graph.python

---
+++ find-if-path-exists-in-graph.py
@@ -0,0 +1,36 @@
+class Solution:
+    def find(self,rep,  x):
+        root = x
+        while root != rep[root]:
+            rep[root] = rep[rep[root]]
+            root = rep[root]
+        
+        while x != root:
+            rep[x], x = root, rep[x]           
+            
+        return root
+
+    def union(self,rep, count, x, y):
+        rep_x = self.find(rep, x)
+        rep_y = self.find(rep, y)
+
+        if rep_x != rep_y:
+            if count[rep_x] > count[rep_y]:
+                rep[rep_y] = rep_x
+                count[rep_x] += count[rep_y]
+            else:
+                rep[rep_x] = rep_y
+                count[rep_y] += count[rep_x]
+
+    def connected(self,rep, x, y):
+        return self.find(rep,x) == self.find(rep,y)
+
+
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        rep = {i : i for i in range(n)}
+        count = [1]*n
+
+        for start, end in edges:
+            self.union(rep, count, start, end)
+
+        return self.connected(rep, source, destination)
