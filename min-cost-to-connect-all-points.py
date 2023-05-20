1a32382d94e411f78be856699861dad493e4349d
Mihret Agegnehu <102969913+mihretgold@users.noreply.github.com>
2023-05-20 06:29:49 +0300

    added min-cost-to-connect-all-points.python

---
+++ min-cost-to-connect-all-points.py
@@ -0,0 +1,51 @@
+class Solution:
+    def find(self,rep,  x):
+        root = x
+        while root != rep[root]:
+            rep[root] = rep[rep[root]]
+            root = rep[root]
+        
+        while x != root:
+            rep[x], x = root, rep[x]           
+            
+        return root
+
+    def union(self,rep, count, x, y):
+        rep_x = self.find(rep, x)
+        rep_y = self.find(rep, y)
+
+        if rep_x != rep_y:
+            if count[rep_x] > count[rep_y]:
+                rep[rep_y] = rep_x
+                count[rep_x] += count[rep_y]
+            else:
+                rep[rep_x] = rep_y
+                count[rep_y] += count[rep_x]
+
+    def connected(self,rep, x, y):
+        return self.find(rep,x) == self.find(rep,y)
+
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        length = len(points)
+        if length == 0:
+            return 0
+        edges = []
+        res = 0
+        rep = {i : i for i in range(length)}
+        count = {i : i for i in range(length)}
+
+        for index in range(length):
+            for i in range(length-1):
+                if index != i:                
+                    x = abs(points[index][0] - points[i][0])
+                    y = abs(points[index][1] - points[i][1])
+                    d = x + y
+                    edges.append((d, index, i))
+                    
+        edges.sort()
+        for dis, start, end in edges:
+            if not self.connected(rep, start, end):
+                res += dis
+                self.union(rep, count, start, end)
+
+        return res
