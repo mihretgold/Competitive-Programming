1cc7950ad6440f820864f1c5de90cacdec58eccf
Mihret Agegnehu <102969913+mihretgold@users.noreply.github.com>
2023-06-11 08:24:43 +0300

    added solving-questions-with-brainpower.python

--- solving-questions-with-brainpower.py
+++ solving-questions-with-brainpower.py
@@ -2,11 +2,13 @@ class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
         length = len(questions)
         dp = [0]*length
+        maxArr = [0]*(length+1)
 
         for index in range(length-1, -1, -1):
             dp[index] += questions[index][0]
             if index + questions[index][1] + 1 < length:
-                dp[index] += dp[index + questions[index][1] + 1]
+                dp[index] += maxArr[index + questions[index][1] + 1]
+            maxArr[index] = max(maxArr[index+1], dp[index])
 
         maxi = max(dp)
         return maxi
