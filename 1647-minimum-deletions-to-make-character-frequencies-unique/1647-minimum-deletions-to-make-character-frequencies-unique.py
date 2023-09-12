734b0f557b2d4ef264151eb622d9a61e5cbb6a6f
Mihret Agegnehu <102969913+mihretgold@users.noreply.github.com>
2023-09-12 05:09:40 +0300

    Time: 124 ms (64.20%), Space: 17.2 MB (23.36%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -2,17 +2,17 @@ class Solution:
     def minDeletions(self, s: str) -> int:
         lettersCount = Counter(s)
         storage = defaultdict(int)
-        # print(lettersCount)
+       
         maxi = 0
         for letter in lettersCount:
             storage[lettersCount[letter]] += 1
             maxi = max(maxi, lettersCount[letter])
-        # print(storage)
+      
         answer = 0
-        # print(maxi)
+    
         for num in range(maxi, 0, -1):
             count = storage[num] - 1
-            # print(count, storage[num])
+            
             if count > 0:
                 answer += count
                 if num > 1:
