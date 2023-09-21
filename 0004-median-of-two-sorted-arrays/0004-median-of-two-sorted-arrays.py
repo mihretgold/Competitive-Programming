1a43feb621bd427e0676372e3cb6932e40da54f3
Mihret Agegnehu <102969913+mihretgold@users.noreply.github.com>
2023-09-21 06:10:45 +0300

    Time: 91 ms (57.84%), Space: 16.7 MB (36.13%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -0,0 +1,80 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        length = len(nums1) + len(nums2)
+        length1 = len(nums1)
+        length2 = len(nums2)
+        idx = (-1, -1)
+        
+        if length & 1:
+            idx = (length//2, -1)
+        else:
+            idx = (length//2, length//2 + 1)
+        
+        i = 0
+        j = 0
+        index = 0
+        found = 0
+        num = [0, 0]
+        foundans = [0, 0]
+        while i < length1 and j < length2:
+            index += 1
+            if nums1[i] <= nums2[j]:
+                if index == length//2:
+                # print(index, i, j, found)                                   
+                    num[0] = nums1[i]
+                    foundans[0] = 1
+                elif index == length//2 + 1:
+                    num[1] = nums1[i]
+                    foundans[1] = 1
+                    break
+               
+                i += 1
+            else:
+                if index == length//2:
+                # print(index, i, j, found)                                   
+                    num[0] = nums2[j]
+                    foundans[0] = 1
+                elif index == length//2 + 1:
+                    num[1] = nums2[j]
+                    foundans[1] = 1
+                    break
+                
+                j += 1
+
+                
+            
+            
+        
+        while i < length1:
+            index += 1
+            if index == length//2 and not foundans[0]:
+                # print(index, i, length)
+                num[0] = nums1[i]
+                
+            elif index == length//2 + 1 and not foundans[1]:
+                num[1] = nums1[i]
+                break
+            i += 1
+                
+        while j < length2:
+            index += 1
+            # print(index, j, length, foundans)
+            if index == length//2 and not foundans[0]:
+                num[0] = nums2[j]
+                
+            elif index == length//2 + 1 and not foundans[1]:
+                num[1] = nums2[j]
+                break
+            
+            j += 1
+        # print(num, idx) 
+        if length & 1:
+            return num[1]
+        else:
+            return (num[0] + num[1]) / 2
+        
+            
+            
+        
+            
+        
