b447d585f0cf3d5d3c15ebed23dc1fadf9b4fce0
Mihret Agegnehu <102969913+mihretgold@users.noreply.github.com>
2023-09-25 08:09:45 +0300

    Time: 38 ms (78.35%), Space: 16.1 MB (96.19%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,20 +1,12 @@
 class Solution:
-#     Algorithm
-# s1. create 2 hash maps and store the count of each 
-# s2. compare the 2 hash maps and display the unique one
-
     def findTheDifference(self, s: str, t: str) -> str:
-        #put the letters and counts in string to dictionary 
-        map_s = defaultdict(int)
-        for letter in s:
-            map_s[letter] += 1           
+        
+        word1 = Counter(s)
+        word2 = Counter(t)
+        
+        for letter in word2:
+            if word2[letter] != word1[letter]:
+                return letter
             
-        map_t = defaultdict(int)
-        for letter in t:
-            map_t[letter] += 1      
-          
-        for compare in map_t:
-            if compare not in map_s or map_t[compare] != map_s[compare]:
-                return compare
-           
+            
             
