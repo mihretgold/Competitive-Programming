552e39306f2eadef0de568c7baa43daabf0833d6
Mihret Agegnehu <102969913+mihretgold@users.noreply.github.com>
2023-10-03 16:26:54 +0300

    added maximum-xor-of-two-numbers-in-an-array.python

--- maximum-xor-of-two-numbers-in-an-array.py
+++ maximum-xor-of-two-numbers-in-an-array.py
@@ -1,20 +1,76 @@
+class TrieNode:
+    def __init__(self):
+        self.kids = [None for _ in range(2)]
+        self.isEnd = False
+
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        current = self.root
+        for ch in word:
+            index = int(ch)
+            if current.kids[index] == None:
+                current.kids[index] = TrieNode()
+
+            current = current.kids[index]
+
+        current.isEnd = True
+
+    def search(self, word: str):
+
+        answer = 0
+        current = self.root
+        for i, ch in enumerate(word):
+            index = int(ch)
+            # print(index ^ 1)
+            if current.kids[index ^ 1] == None:
+                current = current.kids[index]
+            else:
+                answer += 1 << (len(word) - i - 1)
+                current = current.kids[index ^ 1]
+
+        return answer
+
+
+
+
+
+
 class Solution:
     def findMaximumXOR(self, nums: List[int]) -> int:
+        root = {}
+
+        size = len(bin(max(nums))) - 2
+        trie = Trie()
+        length = len(nums)
+       
+        
+
+        answer = 0
+        for num in nums:
+            current = root
+            for i in range(size, -1, -1):
+                index = (num >> i) & 1
+                if index not in current:
+                    current[index] = {}
+    
+                current = current[index]
         maxx = 0
-        mask = 0
-        size = len(bin(max(nums))) - 1 
+        for num in nums:
+            current = root
+            answer = 0
+            for i in range(size, -1, -1):
+                index = (num >> i) & 1
+                if (index ^ 1) not in current:
+                    current = current[index]
+                else:
+                    current = current[index ^ 1]
+                    answer += 1 << i
 
-        for i in range(size, -1, -1):
-            mask |= (1 << i)
-            newmask = maxx | (1 << i)
-
-            xorSet = set( num & mask for num in nums)
-
-            for prefix in xorSet:
-                if prefix ^ newmask in xorSet:
-                    maxx = newmask
-
-            xorSet.clear()
-                    
+            maxx = max(maxx, answer)
 
         return maxx
