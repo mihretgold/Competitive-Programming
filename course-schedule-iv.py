8fd32eeeb49d12b69098dad750d5085e8e001cb3
Mihret Agegnehu <102969913+mihretgold@users.noreply.github.com>
2023-10-04 09:39:15 +0300

    added course-schedule-iv.python

--- course-schedule-iv.py
+++ course-schedule-iv.py
@@ -1,34 +1,23 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        indegree = [0] * numCourses
-        graph = defaultdict(list)
-        check = [set()]*numCourses
-        length = len(queries)
-        result = [False]*length
+        matrix = [[float('inf') for _ in range(numCourses)] for _ in range(numCourses)]
+        answer = []
 
         for a, b in prerequisites:
-            graph[b].append(a)
-            indegree[a] += 1
+            matrix[a][b] = 1
 
-        queue = deque()
         for index in range(numCourses):
-            if indegree[index] == 0:
-                queue.append(index)
+            matrix[index][index] = 0
 
-        while queue:
-            node = queue.popleft()
+        for k in range(numCourses):
+            for row in range(numCourses):
+                for col in range(numCourses):
+                    newDistance = matrix[row][k] + matrix[k][col]
+                    if newDistance < matrix[row][col]:
+                        matrix[row][col] = newDistance
 
-            for child in graph[node]:
-                indegree[child] -= 1
-                check[child] = check[child].union(check[node])
-                check[child].add(node)
-                if indegree[child] == 0:
-                    queue.append(child)
-        
-        
-        for i in range(length):
-            a, b = queries[i]
-            if b in check[a]:
-                result[i] = True
-                
-        return result
+        for a, b in queries:
+            value = matrix[a][b]
+            answer.append(value != float('inf'))
+
+        return answer
