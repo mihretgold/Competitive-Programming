893bdee1dacec24f3845d880baec77447100a728
Mihret Agegnehu <102969913+mihretgold@users.noreply.github.com>
2023-12-02 20:18:27 +0300

    Added solution for Candy

---
+++ leet_code_solutions/candy.py
@@ -0,0 +1,30 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        length = len(ratings)
+        arr = []
+        answer = [0] * length
+        total = 0
+        
+        for index in range(length):
+            arr.append((ratings[index], index))
+            
+        arr.sort()
+        
+        for val, index in arr:
+            left = 0
+            right = 0
+            
+            if index - 1 >= 0 and ratings[index - 1] != val:
+                left = answer[index - 1]
+            
+            if index + 1 < length and ratings[index + 1] != val:
+                right = answer[index + 1]
+                
+            answer[index] = max(left, right) + 1
+            total += answer[index]
+        
+        
+        
+        return total
+                
+                
