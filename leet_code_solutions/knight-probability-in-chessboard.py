290a213134c98ff664e92a93193e4302977bf427
Mihret Agegnehu <102969913+mihretgold@users.noreply.github.com>
2023-12-17 10:19:05 +0300

    Added solution for Knight Probability in Chessboard

---
+++ leet_code_solutions/knight-probability-in-chessboard.py
@@ -0,0 +1,32 @@
+class Solution:
+    def knightProbability(self, n: int, k: int, row: int, column: int) -> float:
+        directions = [(-2, -1), (-2, 1), (-1, -2), (-1, 2), (2, -1), (2, 1), (1, -2), (1, 2)]
+
+        def inbound(row, col):
+            return (0 <= row < n) and (0 <= col < n)
+
+        mem = defaultdict(int)
+        def calcProbablity(moves, row, col):
+            if not inbound(row, col):
+                return 0
+
+            if moves == 0:
+                return 1
+
+            if (moves, row, col) in mem:
+                return mem[(moves, row, col)]
+
+            count = 0
+
+            for newRow, newCol in directions:
+                newRow += row
+                newCol += col
+                count += calcProbablity(moves - 1, newRow, newCol)/8
+
+            mem[(moves, row, col)] = count
+            return mem[(moves, row, col)]            
+        
+        return calcProbablity(k, row, column) 
+
+
+
