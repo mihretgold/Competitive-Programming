74a629f3fe66bc747045a92cdde2b123d99fab22
Mihret Agegnehu <102969913+mihretgold@users.noreply.github.com>
2023-12-20 19:50:16 +0300

    Time: 1793 ms (16.05%), Space: 32.2 MB (5.72%) - LeetHub

---
+++ 2968-apply-operations-to-maximize-frequency-score/2968-apply-operations-to-maximize-frequency-score.py
@@ -0,0 +1,38 @@
+class Solution:
+    def maxFrequencyScore(self, A: List[int], k: int) -> int:
+        prefix = list(accumulate(sorted(A), initial = 0))
+        low = 1
+        high = len(A) 
+        length = len(A)
+        answer = 0
+        A.sort()
+        
+        while low <= high:
+            mid = low + (high - low)//2
+            start = 0
+            for end in range(length):
+                if end - start + 1 > mid:
+                    start += 1
+                if end - start + 1 == mid:
+                    target = (end - start) // 2 + start
+                    targetValue = A[target]
+                    left = (targetValue * (target - start + 1)) - (prefix[target + 1] - prefix[start])
+                    right = (prefix[end+1] - prefix[target + 1]) - (targetValue * (end - target))
+                    # print(prefix)
+                    # print(A)
+                    # print(prefix[end+1] , prefix[target + 1] , (targetValue * (end - target)))
+                    # print(target, targetValue, left, right, mid, end, start)
+                    if left + right <= k:
+                        # print("in")
+                        low = mid + 1
+                        answer = mid
+                        break
+                
+            else:
+                high = mid - 1
+                
+        return answer
+                    
+                    
+                
+        
